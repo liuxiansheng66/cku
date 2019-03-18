03f23ddeb5a120e4efaa072d30acc444aacff286
liuxiansheng66 <2380197840.qqcom>
2019-03-18 10:42:30 +0800

    sdfg

---
+++ jenkins6/src/baidu/baidu01.java
@@ -0,0 +1,35 @@
+package baidu;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class baidu01 {
+
+	public static WebDriver driver;
+	public static String url;
+@BeforeClass
+public void a1() throws MalformedURLException {
+	
+	System.setProperty("webdriver.chrome.driver", "./Tools/chromedriver.exe");
+	driver=new ChromeDriver();
+	
+	/*DesiredCapabilities a=DesiredCapabilities.chrome();
+	WebDriver driver=new RemoteWebDriver(new URL("http://192.168.64.1:4444/hub"),a);
+	*/
+	url="https:www.baidu.com";
+
+}
+@Test
+public void baidu() {
+	driver.get(url);
+}
+@AfterClass   //¹Ø±Õ
+public void a4() {
+	//driver.quit();
+}
+}
